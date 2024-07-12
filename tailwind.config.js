c0010735794b9e5c59f83eed6efe31151f5de9e2
root <root@Duane>
2024-07-12 14:59:27 +0800

    chore: updated port and darkmode setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: '',
+  darkMode: 'media',
   content: ['./index.html', './src/**/*.tsx'],
   theme: {
     extend: {
--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,7 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
+  server: {
+    port: 3000,
+  },
 });
